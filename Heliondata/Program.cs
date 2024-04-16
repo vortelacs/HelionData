ac34d5c7d9b1e81d534df30920cc0f0fb51bb068
Evghenii <evghenii.sirbu@yahoo.com>
2024-04-16 19:43:51 +0300

    configured database connection

--- Heliondata/Program.cs
+++ Heliondata/Program.cs
@@ -1,13 +1,21 @@
+using Heliondata.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<HelionDBContext>(options =>
+options.UseMySQL(builder.Configuration.GetConnectionString("MySqlConnection"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
